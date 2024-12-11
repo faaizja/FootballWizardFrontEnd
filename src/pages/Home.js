1277a13caf0a1e7eee79e26e2bb6acddc2cbed11
harjotsk03 <harjotsk03@gmail.com>
2024-12-11 10:45:43 -0800

    add basic design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { UserMessage } from "../components/UserMessage";
 import { ResponseMessage } from "../components/ResponseMessage";
+import { SideNav } from "../components/SideNav";
 
 export const Home = () => {
   const [input, setInput] = useState("");
@@ -35,34 +36,38 @@ export const Home = () => {
 
   return (
     <div className="p-4">
-      <h1 className="px-4 text-2xl urbanist-semibold">Football Wizard</h1>
-      <div className="rounded-lg w-full p-4 h-96 overflow-y-auto mb-4 flex flex-col gap-3">
-        {messages.map((message, index) => (
-          <div key={index}>
-            {message.user ? (
-              <UserMessage message={message.text} />
-            ) : (
-              <ResponseMessage message={message.text} />
-            )}
+      <div className="flex flex-row">
+        <SideNav />
+        <div className="w-5/6 h-screen fixed top-0 right-0 flex flex-col">
+          <div className="rounded-lg w-full p-4 flex-grow h-auto overflow-y-auto mb-4 flex flex-col gap-3">
+            {messages.map((message, index) => (
+              <div key={index}>
+                {message.user ? (
+                  <UserMessage message={message.text} />
+                ) : (
+                  <ResponseMessage message={message.text} />
+                )}
+              </div>
+            ))}
           </div>
-        ))}
+          {error && <div className="text-red-500 mb-4">{error}</div>}
+          <form onSubmit={handleSubmit} className="flex mb-5 mr-4 ml-4">
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Ask Football Wizard..."
+              className="flex-grow p-2 border rounded-l-lg focus:outline-none focus:ring-2 focus:ring-fuchsia-300"
+            />
+            <button
+              type="submit"
+              className="bg-fuchsia-900 transition duration-500 ease-in-out text-white p-2 rounded-r-lg hover:bg-fuchsia-600 focus:outline-none focus:ring-2 focus:ring-fuchsia-300"
+            >
+              Send
+            </button>
+          </form>
+        </div>
       </div>
-      {error && <div className="text-red-500 mb-4">{error}</div>}
-      <form onSubmit={handleSubmit} className="flex">
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Ask Football Wizard..."
-          className="flex-grow p-2 border rounded-l-lg focus:outline-none focus:ring-2 focus:ring-fuchsia-300"
-        />
-        <button
-          type="submit"
-          className="bg-fuchsia-900 transition duration-500 ease-in-out text-white p-2 rounded-r-lg hover:bg-fuchsia-600 focus:outline-none focus:ring-2 focus:ring-fuchsia-300"
-        >
-          Send
-        </button>
-      </form>
     </div>
   );
 };
